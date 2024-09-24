95a633e776500bfb2a3881806b6e73956d1d476c
Angelo Carlotto <angelocarlotto@gmail.com>
2024-09-24 14:19:34 -0400

    added a tour on index and home page

--- CoopTracker/wwwroot/js/site.js
+++ CoopTracker/wwwroot/js/site.js
@@ -3,3 +3,23 @@
 
 // Write your JavaScript code.
 
+document.getElementById("tourBtn").onclick = () => {
+    if (tour != undefined && tour!=null)
+        tour.start();
+
+}
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
